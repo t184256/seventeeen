8ec119bb95b74b5eb829ba1d752d4889fca0d7b5
Alexander Sosedkin <monk@unboiled.info>
2025-06-21 11:39:21 +0200

    default.nix: enable User.Drawings on svg preview

--- default.nix
+++ default.nix
@@ -60,7 +60,7 @@ stdenvNoCC.mkDerivation {
         $out/pcbs/seventeeen.kicad_pcb \
         --exclude-drawing-sheet \
         --fit-page-to-board \
-        -l F.Cu,B.Cu,F.Paste,B.Paste,F.SilkS,B.SilkS,F.Mask,B.Mask,Edge.Cuts
+        -l F.Cu,B.Cu,F.Paste,B.Paste,F.SilkS,B.SilkS,F.Mask,B.Mask,Edge.Cuts,User.Drawings
   '';
 
   meta = {
